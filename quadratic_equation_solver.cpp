f461ef2a2763f74a0bace93abc656f6e9c9d2da7
Антон Головинский <ragplaychan100@gmail.com>
2024-08-19 00:55:51 +0300

    Fix v1.1
    Function for printing answer and solving linear equations are added. Fixed quadratic function. And now it can get REAL coeffitients.

--- quadratic_equation_solver.cpp
+++ quadratic_equation_solver.cpp
@@ -1,45 +1,75 @@
+#include <TXLib.h>
 #include <stdio.h>
 #include <math.h>
-void solve_qe(int a, int b, int c);
+int solve_qe(double a, double b, double c, double *px1, double *px2);
+int solve_nonqe(double b, double c, double *px1);  // Solve linear equation (a = 0)
+void answer_qe(int number, double x1, double x2);  // Print answer of the quadratic equation
 
-main()
+int main()
 {
-    int a, b, c;
+    int number = 0;  // Number of solutions (3 = infinite)
+    double a = 0; double b = 0; double c = 0;
+    double x1 = 0; double x2 = 0;
 
     printf("Enter the coefficients of quadratic equation:\n");
     printf("a = ");
-    scanf("%d", &a);
+    scanf("%lf", &a);
     printf("b = ");
-    scanf("%d", &b);
+    scanf("%lf", &b);
     printf("c = ");
-    scanf("%d", &c);
+    scanf("%lf", &c);
 
-    solve_qe(a, b, c);
+    number = solve_qe(a, b, c, &x1, &x2);
+    answer_qe(number, x1, x2);
+
     return 0;
 }
 
 
-void solve_qe(int a, int b, int c)
+void answer_qe(int number, double x1, double x2)  // Print answer of the quadratic equation
 {
-    float D = b*b - 4*a*c;
-    if (a == 0) {
-        printf("This equation is not quadratic");
-        return;
-    }
-    if (D < 0) {
-        printf("This equation has no real roots");
-        return;
-    }
-    if (D == 0) {
-        float x = -b/(2*a);
-        printf("This equation has only one real root: %.2f", x);
-        return;
+    if (number == 0)
+        printf("This equation has no real roots\n");
+    else if (number == 1)
+        printf("This equation has only one real root: %.2f\n", x1);
+    else if (number == 2)
+        printf("This equation has only two real roots: %.2f; %.2f\n", x1, x2);
+    else if (number == 3)
+        printf("This equation has an infinite number of real roots\n");
+    else
+        printf("Unknown error\n");
+}
+
+
+int solve_qe(double a, double b, double c, double *px1, double *px2)
+{
+    double D = b*b - 4*a*c;
+    if (abs(a) <= 0.1E-10)
+        return solve_nonqe(b, c, px1);
+    else if (D < 0)
+        return 0;
+    else if (abs(D) <= 0.1E-10) {
+        *px1 = -b/(2*a);
+        return 1;
     }
     else {
-        float x1, x2;
-        x1 = (-b + sqrt(D))/(2*a);
-        x2 = (-b - sqrt(D))/(2*a);
-        printf("This equation has only two real roots: %.2f; %.2f", x1, x2);
-        return;
+        *px1 = (-b + sqrt(D))/(2*a);
+        *px2 = (-b - sqrt(D))/(2*a);
+        return 2;
+    }
+}
+
+
+int solve_nonqe(double b, double c, double *px1)  // Solve linear equation (a = 0)
+{
+    if (abs(b) <= 0.1E-10) {
+        if (abs(c) <= 0.1E-10)
+            return 3;
+        else
+            return 0;
+    }
+    else {
+        *px1 = -c/b;
+        return 1;
     }
 }
